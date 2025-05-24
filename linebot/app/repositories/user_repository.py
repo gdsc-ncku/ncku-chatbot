49fde99ce63f9ee61622ad6789cd6a52932dcf47
yenslife <77geo5rge6@gmail.com>
2025-05-24 21:59:24 +0800

    feat(user_repository.py): add methods to update user attributes

--- linebot/app/repositories/user_repository.py
+++ linebot/app/repositories/user_repository.py
@@ -22,6 +22,24 @@ class UserRepository:
         user.conversation_id = conversation_id
         self.db.commit()
 
+    def update__accpted_terms(self, line_id: str, accepted: bool) -> None:
+        """更新用戶是否接受服務條款"""
+        user = self.get_user(line_id)
+        user.accpted_terms = accepted
+        self.db.commit()
+
+    def update_language(self, line_id: str, language: str) -> None:
+        """更新用戶的語言設定"""
+        user = self.get_user(line_id)
+        user.language = language
+        self.db.commit()
+
+    def update_roles(self, line_id: str, roles: str) -> None:
+        """更新用戶的角色"""
+        user = self.get_user(line_id)
+        user.roles = roles
+        self.db.commit()
+
     def __del__(self):
         """確保資料庫連接被正確關閉"""
         self.db.close()
