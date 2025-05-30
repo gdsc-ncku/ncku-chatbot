85f670733f66cf086e3c89d1bda23941117e185b
marco310 <limarco310@gmail.com>
2025-05-30 22:45:36 +0800

    add github action

--- crawler_testing.py
+++ crawler_testing.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     END_STR = "=END="
 
     start_time = timer()
-    activity_crawler(URL, PATH, END_STR, max_worker=0, headless=True)
+    activity_crawler(URL, PATH, END_STR, max_worker=4, headless=True)
     diff_time(start_time, "Code")
