55beede0d72c46cb18f26a1fa3c9d5101ebcca8d
yenslife <77geo5rge6@gmail.com>
2025-05-31 01:36:41 +0800

    feat(linebot): add redis support for rate limiting
    This commit introduces Redis integration for rate limiting in the Line
    Bot application. It includes the following changes:
    
    - Added Redis configuration parameters (REDIS_HOST, REDIS_PORT,
      REDIS_DB) to the .env.example file.
    - Updated the README.md file to include instructions for installing
      sqlite3 and redis.
    - Modified the redis_client.py file to read Redis configuration from
      environment variables, allowing for flexible deployment.
    
    The addition of Redis enables the implementation of robust rate
    limiting mechanisms, preventing abuse and ensuring the stability of
    the Line Bot service.

--- linebot/app/db/redis_client.py
+++ linebot/app/db/redis_client.py
@@ -1,9 +1,17 @@
+import os
+
 from redis import Redis
+from dotenv import load_dotenv
+
+load_dotenv()
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
 
 redis_client: Redis = Redis(
-    host="localhost",
-    port=6379,
-    db=0,
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=REDIS_DB,
     decode_responses=True,
     retry_on_timeout=True,
     socket_connect_timeout=5,
